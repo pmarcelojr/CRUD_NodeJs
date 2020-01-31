88166c0736772e96959145ac2a5d13a37a513a05
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-01-31 17:51:01 -0300

    mostrar usuarios que foram cadastrados no BD

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const app = express()
 const MongoClient = require('mongodb').MongoClient;
 
 const uri = "mongodb+srv://marcelo:qwer4321@cluster0-q7aco.mongodb.net/crud-nodejs?retryWrites=true&w=majority"
@@ -14,8 +15,6 @@ MongoClient.connect(uri, (err, client) => {
   })
 })
 
-const app = express()
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
@@ -24,15 +23,20 @@ app.get('/', (request, response) => {
   let cursor = db.collection('data').find()
 });
 
-app.post('/show', (request, response) => {
-  db.collection('data').save(request.body, (err, result) => {
+app.get('/show', (request, response) => {
+  db.collection('data').find().toArray((err, results) => {
     if(err)
       return console.log(err)
+      response.render('show.ejs', { data: results })
+  })
+})
 
+app.post('/show', (request, response) => {
+  db.collection('data').save(request.body, (err, result) => {
+    if (err)
+      return console.log(err)
+    
     console.log('Salvo no Banco de Dados')
-    response.redirect('/')
-    db.collection('data').find().toArray((err, results) => {
-      console.log(results)
-    })
+    response.redirect('/show')
   })
 })
